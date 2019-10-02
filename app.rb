d8b5ef811bcea73a3e3babea99333ba7232cd359
Eric Loudon <loudon.eric@gmail.com>
2019-10-02 19:49:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,5 @@
-require_relative 'config/environment'
-require_relative 'models/puppy.rb'
-
 class App < Sinatra::Base
+
   get '/' do
     erb :index
   end
@@ -11,10 +9,8 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    @name = params[:name]
-    @breed = params[:breed]
-    @months_old = params[:age]
-
+    @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
     erb :display_puppy
- end
+  end
+
 end
